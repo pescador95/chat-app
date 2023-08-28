8a91cb78ab9796ad327566803e0bdb8dd10b3b9f
Iédio João Carabolante Júnior <iedio_junior@hotmail.com>
2023-08-28 08:35:09 -0300

    Adicionado comando de Enter para entrar na sala

--- public/scripts/entry.js
+++ public/scripts/entry.js
@@ -1,4 +1,4 @@
-document.getElementById("enter-button").addEventListener("click", function () {
+function redirectToChat() {
   const roomName = document.getElementById("room-name").value;
   const nickname = document.getElementById("nickname").value;
 
@@ -6,4 +6,21 @@ document.getElementById("enter-button").addEventListener("click", function () {
 
   const roomNameValue = document.getElementById("room-name-value");
   roomNameValue.textContent = `Sala: ${roomName}`;
-});
+}
+document
+  .getElementById("enter-button")
+  .addEventListener("click", redirectToChat);
+document
+  .getElementById("nickname")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      redirectToChat();
+    }
+  });
+document
+  .getElementById("room-name")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      redirectToChat();
+    }
+  });
